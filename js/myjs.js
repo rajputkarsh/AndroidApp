4be8f5d3c2f73c9e3bab4854d6fe8e10766fb678
Utkarsh <r.utkarsh.0010@gmail.com>
2020-04-23 04:05:34 +0530

    ajax included

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,114 @@
+ 
+// $(document).ready(function(){
+//   $.ajax({
+//     url : "header.php",
+//     type : "GET",
+
+//     success : function(result){
+//       $("#menuNav").html(result);
+//     },
+//     error : function(xhr, status){
+//       alert("Cannot Display Menu "+xhr+" - "+status);
+//     }
+//   });
+//   $.ajax({
+//     url : "footer.php",
+//     type : "GET",
+
+//     success : function(result){
+//       $("#footer").html(result);
+//     },
+//     error : function(xhr, status){
+//       alert("Cannot Display Footer"+xhr+" - "+status);
+//     }
+//   });
+// });
+
+function getPVC(){
+  document.getElementById("gallery").innerHTML="";
+  $.ajax({url: "pvc.html", success: function(result){
+      $("#gallery").html(result);
+    }});
+}
+
+function getBakelite(){
+  document.getElementById("gallery").innerHTML="";
+  $.ajax({url: "bakelite.html", success: function(result){
+      $("#gallery").html(result);
+    }});
+}
+
+function getUV(){
+  document.getElementById("gallery").innerHTML="";
+  $.ajax({url: "uv.html", success: function(result){
+      $("#gallery").html(result);
+    }});
+}
+
+function getHPL(){
+  document.getElementById("gallery").innerHTML="";
+  $.ajax({url: "hpl.html", success: function(result){
+      $("#gallery").html(result);
+    }});
+}
+
+function getGrass(){
+  document.getElementById("gallery").innerHTML="";
+  $.ajax({url: "grass.html", success: function(result){
+      $("#gallery").html(result);
+    }});
+}
+
+function getFloor(){
+  document.getElementById("gallery").innerHTML="";
+  $.ajax({url: "floor.html", success: function(result){
+      $("#gallery").html(result);
+    }});
+}
+
+function getGypsum(){
+  document.getElementById("gallery").innerHTML="";
+  $.ajax({url: "gypsum.html", success: function(result){
+      $("#gallery").html(result);
+    }});
+}
+
+
+$(document).ready(function(){
+
+    function alignModal(){
+
+        var modalDialog = $(this).find(".modal-dialog");
+
+        /* Applying the top margin on modal dialog to align it vertically center */
+
+        modalDialog.css("margin-top", Math.max(0, ($(window).height() - modalDialog.height()) / 2));
+
+    }
+
+    // Align modal when it is displayed
+
+    $(".modal").on("shown.bs.modal", alignModal);
+
+    
+
+    // Align modal when user resize the window
+
+    $(window).on("resize", function(){
+
+        $(".modal:visible").each(alignModal);
+
+    });   
+
+});
+
+function opening(url){
+    console.log("image clicked");
+  $("#imgbody").html("<img src = '"+url+"' width='100%' height='100%' />");
+  // alert("<img src = '"+url+"' width='100%' height='100%'>");
+  $("#myModal").modal();
+}
+
  $(function(){
     var responsiveWidth = 990;
     var _widthResize;
